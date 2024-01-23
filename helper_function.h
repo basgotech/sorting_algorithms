04264d5c177a9a134f7f6688f97afb7d39c2e4cf
Basliel Tegegn <basgolilo5@gmail.com>
2024-01-23 09:25:34 -0800

    fix helper function

--- helper_function.h
+++ helper_function.h
@@ -45,6 +45,79 @@ return (upper);
 
 
 
+/**
+ * merge_div_arr - Merges two divided arrays into a single array.
+ * @div_arr: The original array to be merged.
+ * @buffer_getter: A buffer array used for merging.
+ * @first: The starting index of the first sub-array.
+ * @center: The ending index of the first sub-array and the starting index of the second sub-array.
+ * @end: The ending index of the second sub-array.
+ *
+ * Description:
+ * - This function merges two divided arrays into a single sorted array.
+ * - The two sub-arrays to be merged are [first, center) and [center, end) in div_arr.
+ * - The buffer_getter array is used as a temporary buffer for merging.
+ * - The function assumes that the two sub-arrays are already sorted.
+ * - The time complexity of this function is O(end - first).
+ * - This function is designed for use in a top-down merge sort algorithm.
+ */
+void merge_div_arr(int *div_arr, int *buffer_getter, size_t first, size_t center,
+		size_t end)
+{
+size_t x, y, z = 0;
+printf("Merging...\n[left]: ");
+print_array(div_arr + first, center - first);
+printf("[right]: ");
+print_array(div_arr + center, end - center);
+for (x = first, y = center; x < center && y < end; z++)
+{
+buffer_getter[z] = (div_arr[x] < div_arr[y]) ? div_arr[x++] : div_arr[y++];
+}
+for (; x < center; x++)
+{
+buffer_getter[z++] = div_arr[x];
+}
+for (; y < end; y++)
+{
+buffer_getter[z++] = div_arr[y];
+}
+for (x = first, z = 0; x < end; x++)
+{
+div_arr[x] = buffer_getter[z++];
+}
+printf("[Done]: ");
+print_array(div_arr + first, end - first);
+}
+
+/**
+ * rec_merge_sort - Recursively sorts a divided array using the merge sort algorithm.
+ * @div_arr: The array to be sorted.
+ * @buffer_getter: A buffer array used for merging.
+ * @first: The starting index of the sub-array to be sorted.
+ * @end: The ending index of the sub-array to be sorted.
+ *
+ * Description:
+ * - This function recursively applies the merge sort algorithm to sort a sub-array.
+ * - The sorted sub-arrays are merged using the merge_div_arr function.
+ * - The function uses the buffer_getter array as a temporary buffer for merging.
+ * - The time complexity of this function is O(n log n), where n is the size of the sub-array.
+ * - This function is designed for use in a top-down merge sort algorithm.
+ */
+void rec_merge_sort(int *div_arr, int *buffer_getter, size_t first, size_t end)
+{
+size_t mid;
+
+if (end - first > 1)
+{
+mid = first + (end - first) / 2;
+rec_merge_sort(div_arr, buffer_getter, first, mid);
+rec_merge_sort(div_arr, buffer_getter, mid, end);
+merge_div_arr(div_arr, buffer_getter, first, mid, end);
+}
+}
+
+
+
 
 /**
  * lomuto_algo_sort - Implement the quicksort algorithm through recursion.
