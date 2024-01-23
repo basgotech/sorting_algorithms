865eaa8508b5d06c1138de5e377dcfd708ddf4e9
fraolketema <138703893+fraolketema@users.noreply.github.com>
2024-01-23 21:17:42 +0300

    Update 102-counting_sort.c

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -2,7 +2,7 @@
 #include "helper_function.h"
 
 /**
- * counting_sort - Sort an array of integers in ascending order
+ * counting_sort - Sort an array of integers in ascending order.
  * @array: An array of integers.
  * @size: The size of the array.
  */
